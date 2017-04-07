2445c0b6259370fb2fa7af10a354d3529aed4552
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 07:56:07 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,15 @@
 var tags = require("./tags");
 
+/**
+ * @module {function} bit-docs-html-highlight-line
+ * @parent plugins
+ *
+ * @description Adds a `@highlight` tag.  Needs to be used after `bit-docs-prettify`.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs){
 	var pkg = require("./package.json");
 	var deps = {};
